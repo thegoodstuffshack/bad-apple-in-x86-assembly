3e60aeb4eec7e97e6dc0c1579ea0e02822fd6b34
thegoodstuffshack <thegoodstuffshack@gmail.com>
2023-11-06 18:51:41 +1100

    boot

--- boot.asm
+++ boot.asm
@@ -11,12 +11,12 @@ background db 32	; ascii space
 start:
 	;load data.bin
 	call read_data_to_memory
-	
+
 	push 0x7e00	; word [bp+6] ; first addr
-	push 225; word [bp+4] ; no.
+	push 70; word [bp+4] ; no.
 	call frame
 	push 0x8000	; word [bp+6] ; first addr
-	push 230; word [bp+4] ; no.
+	push 70; word [bp+4] ; no.
 	call frame
 cli
 hlt
@@ -24,7 +24,7 @@ hlt
 read_data_to_memory:
 	push bp
 	mov bp, sp
-	
+
 	mov ah, 0x02
 	mov al, 1
 	mov ch, 0
@@ -34,7 +34,7 @@ read_data_to_memory:
 	mov es, bx
 	mov bx, 0x7e00
 	int 0x13
-	
+
 	mov ah, 0x02
 	mov al, 1
 	mov ch, 0
@@ -44,33 +44,33 @@ read_data_to_memory:
 	mov es, bx
 	mov bx, 0x8000
 	int 0x13
-	
+
 	; mov al, ah
 	; mov ah, 0x0e
 	; int 0x10
-	
+
 	pop bp
 ret
 
 frame:
 	push bp
 	mov bp, sp
-	
+
 	xor cx, cx
 	mov si, cx
 	mov cx, word [bp+4]
 	mov bx, word [bp+6]
-	
+
 	.loop:
 	cmp cx, 0
 	je .end
 	dec cx
 	push cx
 	push si
-	
+
 	push word [bx+si]	; 0x7e00
 	call shift_print
-	
+
 	pop si
 	pop cx
 	mov bx, word [bp+6]
@@ -88,7 +88,7 @@ shift_print:
 	.loop:
 	cmp cl, 16
 	je .shift_print_end
-	
+
 	mov bx, word [bp+4]
 	shl bx, cl
 	push bx
@@ -96,17 +96,17 @@ shift_print:
 	pop bx
 	inc cl
 	jmp .loop
-	
+
 	.shift_print_end:
 	; mov ax, 0x0e00
 	; int 0x10
 	pop bp
 ret	2
-	
-print: 
+
+print:
 	push bp
 	mov bp, sp
-	
+
 	mov ah, 0x0e
 	mov bx, word [bp+4]
 	cmp bh, 0x80	; 1000 0000 ; 128
