0af18a514ed7f36ef36e9d88d1a3023e682283b7
thegoodstuffshack <142713790+thegoodstuffshack@users.noreply.github.com>
2023-11-26 23:01:22 +1100

    clean boot.asm

--- src/boot.asm
+++ src/boot.asm
@@ -3,7 +3,6 @@
 jmp start
 
 ;; CONSTANTS
-;FRAME_NUMBER		dw	256		; 0x0000 + 0x0100 * x, max of x before cf
 TIMER_ADDRESS 		equ	0x046c	; location of PIT timer count
 RELOAD_VALUE 		equ	0x9b84  ; determines tick speed of PIT
 number_of_frames 	equ	3281	; divide frames by 2
@@ -24,11 +23,6 @@ frame_address	dw 0x7e00	; si+7
 
 ;; CODE
 start:
-    
-	; xor ax, ax
-	; mov ss, ax
-	; mov ds, ax
-	; mov es, ax
 	xor ax, ax
 	mov ds, ax
 	mov si, ax
@@ -38,7 +32,6 @@ start:
 	mov ss, ax
 	mov sp, 0x0000
 	
-	
 	mov byte [BOOT_DRIVE], dl
 	mov al, dl
 	mov ah, 0x0e
@@ -47,11 +40,7 @@ start:
 	call PIT_init
 	call check_disk_parameters
 	;call print_disk_parameters
-	;call load_memory
 	call PIT_timer
-	
-	
-	;call load_frame
 
 	; sets cursor to invisible to remove flickering
 	mov ah, 0x01
@@ -59,22 +48,12 @@ start:
 	mov cl, 0b0000
 	int 0x10
 	
-	
-	; mov ah, 0x0e
-	; mov al, 'i'
-	; int 0x10
-	
 	mov cx, number_of_frames
 run:
 	cmp cx, 0
 	je .end
 	dec cx
 	push cx
-	;call test_print
-	
-	mov ah, 0x0e
-	mov al, 'r'
-	int 0x10
 	
 	call PIT_timer
 	call frame_handler
@@ -89,24 +68,13 @@ run:
 	call frame
 	mov word [frame_address], 0x7e00
 	; assuming 2 frames per sector
-
-
-	;call test_print
+	
 	pop cx
 	jmp run
 	
 	.end:
-	mov ah, 0x0e
-	mov al, 'e'
-	int 0x10
 cli
 hlt
-
-; test_print:
-	; mov ah, 0x0e
-	; mov al, 47
-	; int 0x10
-; ret
 
 reset_cursor:
 	mov ah, 0x02
@@ -126,7 +94,6 @@ ret
 %include "src/pit.asm"
 %include "src/print.asm"
 %include "src/load.asm"
-
 
 jmp $ ; catch in case code decides to run away
 times 510 -($-$$) db 0
