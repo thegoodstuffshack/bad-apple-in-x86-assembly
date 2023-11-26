50516a0b4aaffd4ed746fde56142d7e199b5bd34
thegoodstuffshack <142713790+thegoodstuffshack@users.noreply.github.com>
2023-11-26 23:03:12 +1100

    Update pit.asm

--- src/pit.asm
+++ src/pit.asm
@@ -18,29 +18,17 @@ ret
 
 PIT_timer:			; works on its own in normal_mode
 				; not the 'proper way' to implement pit
-	;mov ah, 0x86
-	;mov al, 0
-	;mov cx, 0x0000		; CX:DX interval in microseconds
-	;mov dx, 0x3000		; roughly 29.54fps on qemu 
-	;int 0x15			; delay between frames
-	
 	.timer:
 	mov ax, [TIMER_ADDRESS]
 	mov bx, ax
 	inc bx
 	
-	.loop:		; real hardware gets stuck in this loop
+	.loop:
 	cmp ax, bx
 	jae .tick		; wait til PIT ticks
 	
 	mov ax, [TIMER_ADDRESS]
-	; mov ah, 0x0e
-	; mov al, 'l'
-	; int 0x10
 	jmp .loop
 
 	.tick:
-	mov ah, 0x0e
-	mov al, 't'		; fails to get here???
-	int 0x10
 ret
