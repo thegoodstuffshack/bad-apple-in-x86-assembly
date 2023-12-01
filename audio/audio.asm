90cdf9660f1617ebcc970e39476b83f70717225b
thegoodstuffshack <thegoodstuffshack@gmail.com>
2023-12-01 19:26:14 +1100

    test audio from A3 to B6

--- audio/audio.asm
+++ audio/audio.asm
@@ -1,14 +1,10 @@
 [bits 16]
-[org 0x7c00]
-jmp start
+[org 0x7c00]	; remove when adding to full program
+jmp start		; desired frequency = 1193181.666... / input value
+				; round to nearest even value (maybe idfk)
 
 ;; CONSTANTS
-
-MIDDLE_C equ 0x11d0
-MIDDLE_D equ 0x0fe0
-MIDDLE_E equ 0x0e24
-MIDDLE_F equ 0x0d58
-MIDDLE_G equ 0x0be4
+%include "note_defs.data"
 
 ;; VARIABLES
 
@@ -17,67 +13,32 @@ MIDDLE_G equ 0x0be4
 
 start:
 	call test
-	call pc_speaker_init ; starting spk here causes lengthened first note
-	
-	.loop:
-	mov ax, MIDDLE_C
-	call hz_change
-	call sound
-	call delay
-	
-	mov ax, MIDDLE_D
-	call sound
-	call delay
-	
-	mov ax, MIDDLE_E
-	call hz_change
-	call sound
-	call delay
-	
-	mov ax, MIDDLE_F
-	call hz_change
-	call sound
-	call delay
-	
-	mov ax, MIDDLE_G
-	call hz_change
-	call sound
-	call delay
-	
-	call stop
-	call delay
-	
-	jmp start
+	call speaker_init
+
+	mov si, LOW_A
+	call music_player
 cli
 hlt
 
-hz_change:
-	cli
-	out 0x42, al
-	mov al, ah
-	out 0x42, al
-	sti
+music_player:
+	call on
+
+	.loop:
+	cmp si, HIGH_B
+	ja .end
+
+	mov ax, [si]
+	call hz_change
+	call delay
+
+	add si, 2
+	jmp .loop
+
+	.end:
+	call off
 ret
 
-test:
-	mov ah, 0x0e
-	mov al, 1
-	int 0x10
-ret
-
-sound:
-	in al, 0x61
-	or al, 0b00000011
-	out 0x61, al
-ret
-
-stop:
-	in al, 0x61
-	and al, 0b11111100
-	out 0x61, al
-ret
-
-delay:
+delay:	; make from PIT instead
 	mov ah, 0x86
 	mov al, 0
 	mov cx, 0x0002
@@ -85,19 +46,37 @@ delay:
 	int 0x15
 ret
 
-; desired frequency = 1193181.666... / input value
-; round to nearest even value (maybe idfk)
-
-pc_speaker_init:
+hz_change:	; needs ax input
 	cli
-	mov al, 0b10110110 ; channel 2, hi/lo, mode 3
-	out 0x43, al
-
-	mov ax, 0x11D0	; input value
 	out 0x42, al
 	mov al, ah
 	out 0x42, al
 	sti
+ret
+
+on:	; enable speaker
+	in al, 0x61
+	or al, 0b00000011
+	out 0x61, al
+ret
+
+off:		; disable speaker
+	in al, 0x61
+	and al, 0b11111100
+	out 0x61, al
+ret
+
+speaker_init:
+	cli
+	mov al, 0b10110110 ; channel 2, hi/lo, mode 3
+	out 0x43, al
+	sti
+ret
+
+test:
+	mov ah, 0x0e
+	mov al, 1
+	int 0x10
 ret
 
 jmp $
--- audio/music.py
+++ audio/music.py
@@ -1,21 +1,61 @@
+from math import floor
+from math import ceil
+
+
 # https://pages.mtu.edu/~suits/NoteFreqCalcs.html
+n = -21
+BF = 14318180
 
-n = -9 # where n is the no. of semitones 
-# away from A4 (-ve for below, +ve for above)
-a = 2 ** (1/12)
-print(a)
-desired_value = 440 * (a**n)
-print(desired_value)
+notes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+octaves = ['lorem_ipsum', 'LOW', 'MID', 'UPP', 'HIGH']
+count = m = 0
+o = 1
 
-# round value to nearest even integer
-next_even = desired_value % 2 
-if next_even < 1
-    round down desired value
-else
-    round up desired value
+dict = dict([])
 
-BF = 14318180 # base frequency
-IF = 12 * desired_value # input frequency
-print(IF)
-OUT = BF / IF # value sent to PIT
-print(OUT)
+# Generate the pre
+while count < 48:
+    note = octaves[o] + '_' + notes[m] + '\tdw '
+    #print(note)
+    
+    if count == 11:
+        o = 2
+    elif count == 23:
+        o = 3
+    elif count == 35:
+        o = 4
+    
+    m += 1
+    if m >= 12:
+        m = 0
+
+    freq = 440 * (2**(1/12))**n
+    print(freq)
+
+    input_Hz = BF / freq
+
+    even_num = input_Hz % 2
+    if even_num == 1:
+        input_Hz += 1
+    if even_num < 1:
+        input_Hz = floor(input_Hz)
+    else:
+        input_Hz = ceil(input_Hz)
+
+    print(input_Hz)
+
+
+    final = note + hex(input_Hz)
+    print(final)
+    dict[count] = final
+
+    n += 1
+    count += 1
+    
+# for d in dict:
+#     print(dict[d])
+dest = "note_defs.data"
+
+with open(dest, 'w') as dest:
+    for d in dict:
+        dest.write(dict[d] + '\n')
